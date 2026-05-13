936504f424d44cdb9e766d071b72f2f9e03a8bcb
yonggyu <yonggyu1234@naver.com>
2026-05-13 11:44:29 +0900

    ApiResponse 작성

---
+++ src/main/java/io/github/yonggyu/ecommerce/common/response/ApiResponse.java
@@ -0,0 +1,34 @@
+package io.github.yonggyu.ecommerce.common.response;
+
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+    public ApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+    public static <T> ApiResponse<T> fail(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+}

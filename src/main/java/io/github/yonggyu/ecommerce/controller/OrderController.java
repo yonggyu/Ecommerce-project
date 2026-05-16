50cc467f2d27f35237bbbec1242e995c2564530a
yonggyu <yonggyu1234@naver.com>
2026-05-16 13:11:04 +0900

    생성자 추가

--- src/main/java/io/github/yonggyu/ecommerce/controller/OrderController.java
+++ src/main/java/io/github/yonggyu/ecommerce/controller/OrderController.java
@@ -1,36 +1,34 @@
 package io.github.yonggyu.ecommerce.controller;
 
 import io.github.yonggyu.ecommerce.common.response.BaseResponse;
+import io.github.yonggyu.ecommerce.dto.order.OrderCreateRequest;
 import io.github.yonggyu.ecommerce.dto.order.OrderCreateResponse;
 import io.github.yonggyu.ecommerce.dto.order.OrderResponse;
+import io.github.yonggyu.ecommerce.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 @RestController
 @RequestMapping("/api/orders")
 @Tag(name = "Order API", description = "주문 API")
 public class OrderController {
 
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @PostMapping
     @Operation(summary = "주문 생성")
     @ApiResponse(responseCode = "200", description = "주문 생성 성공")
-    public ResponseEntity<BaseResponse<OrderCreateResponse>> createOrder() {
-
-        OrderCreateResponse response = new OrderCreateResponse(
-                1L,
-                1L,
-                2,
-                new BigDecimal("3180000"),
-                "CREATED",
-                LocalDateTime.now()
-        );
+    public ResponseEntity<BaseResponse<OrderCreateResponse>> createOrder(
+            @RequestBody(required = false) OrderCreateRequest request
+    ) {
+        OrderCreateResponse response = orderService.createOrder(request);
 
         return ResponseEntity.ok(
                 BaseResponse.success("주문 생성 성공", response)
@@ -43,15 +41,7 @@ public class OrderController {
     public ResponseEntity<BaseResponse<OrderResponse>> getOrderDetail(
             @PathVariable Long orderId
     ) {
-
-        OrderResponse response = new OrderResponse(
-                orderId,
-                11L,
-                "MacBook Air M4",
-                1,
-                new BigDecimal("1590000"),
-                LocalDate.now().atStartOfDay()
-        );
+        OrderResponse response = orderService.getOrderDetail(orderId);
 
         return ResponseEntity.ok(
                 BaseResponse.success("주문 상세 조회 성공", response)

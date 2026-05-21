8c9c8e2b84c08eed736cae446221cca3ee5be22f
yonggyu <yonggyu1234@naver.com>
2026-05-21 13:29:12 +0900

    기타 변경

--- src/main/java/io/github/yonggyu/ecommerce/common/response/BaseResponse.java
+++ src/main/java/io/github/yonggyu/ecommerce/common/response/BaseResponse.java
@@ -1,19 +1,15 @@
 package io.github.yonggyu.ecommerce.common.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class BaseResponse<T> {
 
     private final boolean success;
     private final String message;
     private final T data;
-
-    public BaseResponse(boolean success, String message, T data) {
-        this.success = success;
-        this.message = message;
-        this.data = data;
-    }
 
     public static <T> BaseResponse<T> success(String message, T data) {
         return new BaseResponse<>(true, message, data);

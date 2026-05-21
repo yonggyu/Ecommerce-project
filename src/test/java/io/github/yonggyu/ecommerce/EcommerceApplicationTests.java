0f36addb7decb9bcffd8d60177752c0cd7451bc8
yonggyu <yonggyu1234@naver.com>
2026-05-21 13:28:30 +0900

    JPA 구조로 변경

--- src/test/java/io/github/yonggyu/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/io/github/yonggyu/ecommerce/EcommerceApplicationTests.java
@@ -3,10 +3,7 @@ package io.github.yonggyu.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-		"spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration,"
-				+ "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration"
-})
+@SpringBootTest
 class EcommerceApplicationTests {
 
 	@Test
